caad8d9bfe6928b0144616fb31cd88fe54716e3c
Akshat-iitk <akshatt21@iitk.ac.in>
2024-05-13 22:52:03 +0530

    created a basic Home ContactUs and About page using ReactRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-const router = createBrowserRouter([
-  
-])
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import ContactUs from './components/ContactUs/ContactUs.jsx'
+// const router = createBrowserRouter([
+//   {
+//     path : "/",
+//     element : <Layout/>,
+//     children:[
+//       {
+//         path : "",
+//         element : <Home/>
+//       },
+//       {
+//         path : "about",
+//         element:<About/>
+//       },
+//       {
+//         path : "contact",
+//         element:<ContactUs/>
+//       }
+//     ]
+//   }
+// ]) // one way of making routes 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='/' element={<Home/>} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<ContactUs />} />
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
